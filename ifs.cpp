c5ed4de7ddcac0cd8df6f99ea39536262e078d92
Alonso Rubén Maya Martínez <39639835+Alonsormm@users.noreply.github.com>
2020-02-27 16:20:22 -0600

    Se quedo bien picado el Kevin

--- ifs.cpp
+++ ifs.cpp
@@ -4,9 +4,12 @@
 #include <fstream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#define MAXIMO 200
+#define MAXIMO 100
 using namespace std;
 using namespace cv;
+
+//compilar con g++ -I/usr/local/Cellar/opencv/4.2.0_1/include/opencv4/opencv -I/usr/local/Cellar/opencv/4.2.0_1/include/opencv4 -L/usr/local/Cellar/opencv/4.2.0_1/lib -lopencv_gapi -lopencv_stitching -lopencv_aruco -lopencv_bgsegm -lopencv_bioinspired -lopencv_ccalib -lopencv_dnn_objdetect -lopencv_dnn_superres -lopencv_dpm -lopencv_highgui -lopencv_face -lopencv_freetype -lopencv_fuzzy -lopencv_hfs -lopencv_img_hash -lopencv_line_descriptor -lopencv_quality -lopencv_reg -lopencv_rgbd -lopencv_saliency -lopencv_sfm -lopencv_stereo -lopencv_structured_light -lopencv_phase_unwrapping -lopencv_superres -lopencv_optflow -lopencv_surface_matching -lopencv_tracking -lopencv_datasets -lopencv_text -lopencv_dnn -lopencv_plot -lopencv_videostab -lopencv_videoio -lopencv_xfeatures2d -lopencv_shape -lopencv_ml -lopencv_ximgproc -lopencv_video -lopencv_xobjdetect -lopencv_objdetect -lopencv_calib3d -lopencv_imgcodecs -lopencv_features2d -lopencv_flann -lopencv_xphoto -lopencv_photo -lopencv_imgproc -lopencv_core -std=c++11 ifs.cpp
+
 
 string generarCadenaIfs(int numeroTab)
 {
@@ -17,7 +20,7 @@ string generarCadenaIfs(int numeroTab)
     }
     return temp;
 }
-
+string reanalizar(string cadenaFinal);
 void generarCadena(string &cadenaFinal, double &i)
 {
     double max = MAXIMO;
@@ -32,30 +35,73 @@ void generarCadena(string &cadenaFinal, double &i)
             i--;
             continue;
         }
-        if (cadenaFinal[i] == 'A')
-        {
-            random = rand() % 2;
-            if (random)
+        random = rand() % 2;
+
+        if(random){
+            if (cadenaFinal[i] == 'A')
             {
-                cadenaFinal.replace(i, 1, "(;eS)");
-                parentesisSaltado++;
-                i += 4;
-            }
-            else
-            {
-                cadenaFinal.replace(i, 1, "");
+                random = rand()%2;
+                if (random)
+                {
+                    cadenaFinal.replace(i, 1, "(;eS)");
+                    parentesisSaltado++;
+                    i += 4;
+                }
+                else
+                {
+                    cadenaFinal.replace(i, 1, ")");
+                }
             }
         }
         if (cadenaFinal[i] == 'S')
         {
-            cadenaFinal.replace(i, 1, "(iCtSA)");
+
+                cadenaFinal.replace(i, 1, "(iCtSA)");
+
+
+
             parentesisSaltado++;
             i += 6;
         }
         i--;
         count++;
     }
+    cout << cadenaFinal << '\n';
+
+for(int k=0;k<4;k++)
+    cadenaFinal=reanalizar(cadenaFinal);
+
+
+
+
+
+    cout << cadenaFinal;
+
     i += parentesisSaltado;
+}
+
+string reanalizar(string cadenaFinal){
+    int random;
+        for(int i = 0; i < cadenaFinal.size(); i++){
+        if(cadenaFinal[i] == 'A'){
+            random = rand() %2 ;
+            if(random) {
+                cadenaFinal.replace(i, 1, "(;eS)");
+            }
+            else{
+                cadenaFinal.replace(i, 1, "");
+            }
+        }
+    }
+    for(int i = 0; i < cadenaFinal.size(); i++){
+        if(cadenaFinal[i] == 'S'){
+            random = rand() %2 ;
+            if(random) {
+                cadenaFinal.replace(i, 1, "(iCtSA)");
+            }
+        }
+    }
+    return cadenaFinal;
 }
 
 void generarTxt(string &cadenaFinal, double i, Mat &image)
@@ -63,15 +109,15 @@ void generarTxt(string &cadenaFinal, double i, Mat &image)
 
     unsigned char value1, value2, value3;
     int frame_width = MAXIMO;
-    int frame_height = MAXIMO * 4;
-    VideoWriter video("outcpp.avi", VideoWriter::fourcc('m', 'p', '4', 'v'), 10, Size(frame_width, frame_height));
+    int frame_height = MAXIMO * 7;
+    VideoWriter video("outcpp.mp4", VideoWriter::fourcc('m', 'p', '4', 'v'), 60, Size(frame_height,frame_width));
 
     int row, col = 0;
     double tabulaciones = 0;
     std::ofstream outfile("ifs.txt", std::ofstream::out);
     namedWindow("A_good_name", WINDOW_AUTOSIZE);
 
-    for (int j = 0; j < i; j++)
+    for (int j = 0; j < cadenaFinal.size(); j++)
     {
         if (cadenaFinal[j] == 'i')
         {
@@ -125,15 +171,20 @@ void generarTxt(string &cadenaFinal, double i, Mat &image)
             image.at<Vec3b>(col, i)[2] = value3;
         }
         col++;
+        rotate(image, image, ROTATE_90_COUNTERCLOCKWISE);
         video.write(image);
-        imshow("A_good_name", image);
+        rotate(image, image, ROTATE_90_CLOCKWISE);
+
     }
+        rotate(image, image, ROTATE_90_COUNTERCLOCKWISE);
+
+    imwrite("imagen.jpg", image);
     destroyWindow("A_good_name");
 }
 
 int main()
 {
-    Mat img(MAXIMO * 4, MAXIMO, CV_8UC3, Scalar(255, 255, 255));
+    Mat img(MAXIMO*7,MAXIMO, CV_8UC3, Scalar(255, 255, 255));
     string cadenaFinal = "iCtSA";
     double tam = 5;
     double i = tam - 1;
